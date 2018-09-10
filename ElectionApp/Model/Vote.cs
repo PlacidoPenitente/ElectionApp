697c4b3a601a7462bd53e8b6a4b549a96e6e7eca
Jay Mark Estrera <estrerajaymark@ymail.com>
2018-09-11 01:26:32 +0800

    Updated vote.

--- ElectionApp/Model/Vote.cs
+++ ElectionApp/Model/Vote.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public Candidate Candidate { get; set; }
+        public Position Position { get; set; }
         public Ballot Ballot { get; set; }
     }
 }

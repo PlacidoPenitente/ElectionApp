4fce4047a612726fde53191d1d4ec23f7e90999b
Jay Mark Estrera <estrerajaymark@ymail.com>
2018-09-11 01:46:32 +0800

    Added Facade.

---
+++ ElectionApp/Facade/CandidateFacade.cs
@@ -0,0 +1,34 @@
+﻿using ElectionApp.Model;
+
+namespace ElectionApp.Facade
+{
+    public class CandidateFacade : VoterFacade
+    {
+        private readonly Candidate _candidate;
+
+        public CandidateFacade(Candidate candidate) : base(candidate)
+        {
+            _candidate = candidate;
+        }
+
+        public Position Position
+        {
+            get => _candidate.Position;
+            set
+            {
+                _candidate.Position = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Party Party
+        {
+            get => _candidate.Party;
+            set
+            {
+                _candidate.Party = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
---
+++ ElectionApp/Facade/VoterFacade.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using ElectionApp.Model;
+
+namespace ElectionApp.Facade
+{
+    public class VoterFacade : BaseInpc
+    {
+        private readonly Voter _voter;
+
+        public VoterFacade(Voter voter)
+        {
+            _voter = voter;
+        }
+
+        public string FirstName
+        {
+            get => _voter.FirstName;
+            set
+            {
+                _voter.FirstName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string MiddleName
+        {
+            get => _voter.MiddleName;
+            set
+            {
+                _voter.MiddleName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string LastName
+        {
+            get => _voter.LastName;
+            set
+            {
+                _voter.LastName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DateTime Birthdate
+        {
+            get => _voter.Birthdate;
+            set
+            {
+                _voter.Birthdate = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Address
+        {
+            get => _voter.Address;
+            set
+            {
+                _voter.Address = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Username
+        {
+            get => _voter.Username;
+            set
+            {
+                _voter.Username = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Password
+        {
+            get => _voter.Password;
+            set
+            {
+                _voter.Password = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public AccountType AccountType
+        {
+            get => _voter.AccountType;
+            set
+            {
+                _voter.AccountType = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Photo
+        {
+            get => _voter.Photo;
+            set
+            {
+                _voter.Photo = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
